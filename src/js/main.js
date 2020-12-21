496536154bacfb15c10ce3025eaec1fdbadbee65
Vladyslav Trundenko <fixedfix921@gmail.com>
2020-12-22 01:22:00 +0200

    vlad

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,12 @@
 import './helpers/postDate';
 import scrollSmooth from './helpers/smooth-scroll.js';
 import animation from './modules/animation.js';
-
+import illness from './modules/illness.js';
 
 function main() {
 	scrollSmooth();
 	animation();
-
+	illness()
 }
 
 if (document.documentElement.clientWidth < 480) {
